ba1ea7efdaf57286d112fe481fdcb34f49df71e7
Saurabh <saurabhsingh4778698@gmail.com>
2021-05-20 15:09:12 +0530

    minnor changes

--- script.js
+++ script.js
@@ -15,25 +15,31 @@ function stopwatch() {
   if (now == true) {
     count =count+ 1;
     document.getElementById("count").innerHTML = count;
-    if (count == 25500) {
+    if (count == 100) {
       sec =sec + 1;
       
       document.getElementById("sec").innerHTML = sec;
       count = 0;
+      document.getElementById("count").innerHTML = count;
     }
     if (sec == 60) {
       min =min + 1;
       sec = 0;
       count=0;
+      document.getElementById("sec").innerHTML = sec;
+      document.getElementById("count").innerHTML = count;
       document.getElementById("min").innerHTML = min;
     }
     if (min == 60) {
       hr =hr + 1;
-      now=false;
+      // now=false;
       min = 0;
       sec = 0;
       count=0;
       document.getElementById("hr").innerHTML = hr;
+      document.getElementById("sec").innerHTML = sec;
+      document.getElementById("count").innerHTML = count;
+      document.getElementById("min").innerHTML = min;
     }
     
     setInterval("stopwatch()", 10);
